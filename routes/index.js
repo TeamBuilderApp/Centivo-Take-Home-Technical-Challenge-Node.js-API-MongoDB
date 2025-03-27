d9adc39e5c88564e933769f1b0cecf2852d77b79
Christopher Peterson <teambuilderapp2@gmail.com>
2025-03-27 12:55:08 -0400

    Added User HTTP Web Methods. GET, PUT, POST, and DELETE. Todo, modify the resource MongoDB by User ID. Then a good code clean-up. along with the remainder of Todo for this User Admin Panel.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Centivo Technical Challenge' });
+  res.render('index', { title: 'GET index for Centivo Technical Challenge' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('GET user resource for Centivo Technical Challenge /users list.');
+});
+
+router.post('/', function(req, res, next) {
+  //res.render('index', { title: 'Centivo Technical Challenge' });
+  //res.render('index', { title: 'testing user 3 2 1 POST!' });
+  res.send('POST: User resource for Centivo Technical Challenge. /user 3 2 1 POST!');
+});
+
+router.put('/updateuser', (req, res) => {
+  res.send('PUT: User resource for Centivo Technical Challenge. /user. Todo update a User by ID.')
+});
+
+router.delete('/deleteuser', (req, res) => {
+  res.send('DELETE: User resource for Centivo Technical Challenge. /user. Todo delete a User by ID.')
 });
 
 module.exports = router;
